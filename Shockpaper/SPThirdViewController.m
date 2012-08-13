86c58deac544e6432bf273b4492c5a4283bd738e
Chai Yu Pai <fantasyatelier@gmail.com>
2012-08-14 05:52:08 +0800

    Restore

--- Shockpaper/SPThirdViewController.m
+++ Shockpaper/SPThirdViewController.m
@@ -7,22 +7,12 @@
 //
 
 #import "SPThirdViewController.h"
-#import "SPImageViewController.h"
-#import "SPAppDelegate.h"
 
-static NSUInteger kNumberOfPages = 6;
+@interface SPThirdViewController ()
 
-static NSString *NameKey = @"nameKey";
-static NSString *ImageKey = @"imageKey";
-
-
-@interface SPThirdViewController (PrivateMethods)
-- (void)loadScrollViewWithPage:(int)page;
-- (void)scrollViewDidScroll:(UIScrollView *)sender;
 @end
-@implementation SPThirdViewController
 
-@synthesize scrollView, pageControl, viewControllers;
+@implementation SPThirdViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -31,51 +21,16 @@ static NSString *ImageKey = @"imageKey";
         // Custom initialization
         self.title = NSLocalizedString(@"Third", @"Third");
         self.tabBarItem.image = [UIImage imageNamed:@"second"];
-        
-        
-        
     }
     return self;
 }
 
--(void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"content_iPhone" ofType:@"plist"];
-    self.contentList = [NSArray arrayWithContentsOfFile:path];
-    
-    // view controllers are created lazily
-    // in the meantime, load the array with placeholders which will be replaced on demand
-    NSMutableArray *controllers = [[NSMutableArray alloc] init];
-    for (unsigned i = 0; i < kNumberOfPages; i++)
-    {
-        [controllers addObject:[NSNull null]];
-    }
-    self.viewControllers = controllers;
-    
-    
-    // a page is the width of the scroll view
-    scrollView.pagingEnabled = YES;
-    scrollView.contentSize = CGSizeMake(scrollView.frame.size.width * kNumberOfPages, scrollView.frame.size.height);
-    scrollView.showsHorizontalScrollIndicator = NO;
-    scrollView.showsVerticalScrollIndicator = NO;
-    scrollView.scrollsToTop = NO;
-    scrollView.delegate = self;
-    
-    
-    
-    pageControl.numberOfPages = kNumberOfPages;
-    pageControl.currentPage = 0;
-    
-    // pages are created on demand
-    // load the visible page
-    // load the page on either side to avoid flashes when the user starts scrolling
-    //
-    [self loadScrollViewWithPage:0];
-    [self loadScrollViewWithPage:1];
-
-    // [pageControl addTarget:self action:@selector(changePage:) forControlEvents:UIControlEventValueChanged];
+    // Do any additional setup after loading the view from its nib.
 }
+
 - (void)viewDidUnload
 {
     [super viewDidUnload];
@@ -87,96 +42,5 @@ static NSString *ImageKey = @"imageKey";
 {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
-
-- (void)loadScrollViewWithPage:(int)page
-{
-    if (page < 0)
-        return;
-    if (page >= kNumberOfPages)
-        return;
-    
-    NSLog(@"page: %d",page);
-    // replace the placeholder if necessary
-    SPImageViewController *controller = [viewControllers objectAtIndex:page];
-     if ((NSNull *)controller == [NSNull null])
-     {
-     controller = [[SPImageViewController alloc] initWithPageNumber:page];
-     [viewControllers replaceObjectAtIndex:page withObject:controller];
-    
-     }
-     
-     // add the controller's view to the scroll view
-     if (controller.view.superview == nil)
-     {
-     CGRect frame = scrollView.frame;
-     frame.origin.x = frame.size.width * page;
-     frame.origin.y = 0;
-     controller.view.frame = frame;
-     [scrollView addSubview:controller.view];
-     
-     NSDictionary *numberItem = [self.contentList objectAtIndex:page];
-     controller.numberImage.image = [UIImage imageNamed:[numberItem valueForKey:ImageKey]];
-     controller.numberTitle.text = [numberItem valueForKey:NameKey];
-     }
-}
-
-- (void)scrollViewDidScroll:(UIScrollView *)sender
-{
-    // We don't want a "feedback loop" between the UIPageControl and the scroll delegate in
-    // which a scroll event generated from the user hitting the page control triggers updates from
-    // the delegate method. We use a boolean to disable the delegate logic when the page control is used.
-    if (pageControlUsed)
-    {
-        // do nothing - the scroll was initiated from the page control, not the user dragging
-        return;
-    }
-    
-    // Switch the indicator when more than 50% of the previous/next page is visible
-    CGFloat pageWidth = scrollView.frame.size.width;
-    int page = floor((scrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
-    pageControl.currentPage = page;
-    
-    // load the visible page and the page on either side of it (to avoid flashes when the user starts scrolling)
-    [self loadScrollViewWithPage:page - 1];
-    [self loadScrollViewWithPage:page];
-    [self loadScrollViewWithPage:page + 1];
-    
-    // A possible optimization would be to unload the views+controllers which are no longer visible
-}
-
-// At the begin of scroll dragging, reset the boolean used when scrolls originate from the UIPageControl
-- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
-{
-    pageControlUsed = NO;
-}
-
-// At the end of scroll animation, reset the boolean used when scrolls originate from the UIPageControl
-- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
-{
-    pageControlUsed = NO;
-}
-
-- (IBAction)changePage:(id)sender
-{
-   // UIView * newView = [views objectAtIndex:[pageControl currentPage]];
-    int page = pageControl.currentPage;
-	
-    // load the visible page and the page on either side of it (to avoid flashes when the user starts scrolling)
-    [self loadScrollViewWithPage:page - 1];
-    [self loadScrollViewWithPage:page];
-    [self loadScrollViewWithPage:page + 1];
-    
-	// update the scroll view to the appropriate page
-    CGRect frame = scrollView.frame;
-    frame.origin.x = frame.size.width * page;
-    frame.origin.y = 0;
-    [scrollView scrollRectToVisible:frame animated:YES];
-    
-	// Set the boolean used when scrolls originate from the UIPageControl. See scrollViewDidScroll: above.
-    pageControlUsed = YES;
-   //[self animateToView:newView];
-}
-
-
 
 @end
